c76580683c57d9b15b86fb7f0034d046e281187b
rakshith22-glitch <rakshith22@outlook.com>
2022-06-29 11:26:45 -0700

    origin

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ app.get('/', (req, res) => res.send('Hello Thanks for coming here!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
 
+app.use(cors({
+    origin: "*",
+}));
 
-const whitelist = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://stacklinebackend.herokuapp.com",
-    "https://stacklinedemo.herokuapp.com/",
-  ];
-  const corsOptions = {
-    origin: function (origin, callback) {
-      console.log("** Origin of request " + origin);
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        console.log("Origin acceptable");
-        callback(null, true);
-      } else {
-        console.log("Origin rejected");
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  };
-  app.use(cors(corsOptions));
+// const whitelist = [
+//     "http://localhost:3000",
+//     "http://localhost:3001",
+//     "https://stacklinebackend.herokuapp.com",
+//     "https://stacklinedemo.herokuapp.com/",
+//   ];
+//   const corsOptions = {
+//     origin: function (origin, callback) {
+//       console.log("** Origin of request " + origin);
+//       if (whitelist.indexOf(origin) !== -1 || !origin) {
+//         console.log("Origin acceptable");
+//         callback(null, true);
+//       } else {
+//         console.log("Origin rejected");
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   };
+
 
 let rawdata = fs.readFileSync('data.json');
 let student = JSON.parse(rawdata);
